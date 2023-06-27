aff68d8c57bc78a0c21feb2a17921cc74fd8c0c9
Q0111 <115653327+Q0111@users.noreply.github.com>
2023-06-27 15:09:00 +0700

    Update MQTT.py
    fixed bug

--- MQTT.py
+++ MQTT.py
@@ -1,38 +1,72 @@
+print("MQTT with Adafruit IO")
 import time
 import random
 import sys
 from Adafruit_IO import MQTTClient
+import requests
 
-AIO_FEED_ID = {"Button 1", "Button 2"}
 AIO_USERNAME = "HTQ"
 AIO_KEY = ""
 
-def connected(client):
-    print("Ket noi thanh cong ...")
-    client.subscribe("Button 1")
-    client.subscribe("Button 2")
+global_equation = "x1 + x2 + x3"
 
-def subscribe(client , userdata , mid , granted_qos):
-    print("Subscribe thanh cong ...")
+
+def init_global_equation():
+    headers = {}
+    aio_url = "API"
+    x = requests.get(url=aio_url, headers=headers, verify=False)
+    data = x.json()
+    global_equation = data["last_value"]
+    print("Get lastest value:", global_equation)
+
+
+def modify_value(x1, x2, x3):
+    result = eval(global_equation)
+    print(f"result = {result}")
+    return result
+
+
+def connected(client):
+    print("Server connected ...")
+    client.subscribe("button 1")
+    client.subscribe("button 2")
+    client.subscribe("equation")
+
+
+def subscribe(client, userdata, mid, granted_qos):
+    print("Subscribeb!!!")
+
 
 def disconnected(client):
-    print("Ngat ket noi ...")
-    sys.exit (1)
+    print("Disconnected from the server!!!")
+    sys.exit(1)
 
-def message(client , feed_id , payload):
-    print("Nhan du lieu: " + payload)
 
-client = MQTTClient(AIO_USERNAME , AIO_KEY)
-client.on_connect = connected
+def message(client, feed_id, payload):
+    print("Received: " + payload)
+    if feed_id == "equation":
+        global_equation = payload
+        print(global_equation)
+
+
+client = MQTTClient(AIO_USERNAME, AIO_KEY)
+
+client.on_connect = connected  # callback
 client.on_disconnect = disconnected
 client.on_message = message
 client.on_subscribe = subscribe
+
 client.connect()
 client.loop_background()
+init_global_equation()
 
 while True:
-    time.sleep(5)
-    client.publish("Sensor 1", random.randint(0, 100))
-    client.publish("Sensor 2", random.randint(0, 100))
-    client.publish("Sensor 3", random.randint(0, 100))
+    time.sleep(10)
+    s1 = random.randint(20, 70)
+    s2 = random.randint(0, 100)
+    s3 = random.randint(0, 14)
+    client.publish("sensor1", s1)
+    client.publish("sensor2", s2)
+    client.publish("sensor3", s3)
+    s4 = modify_value(s1, s2, s3)
     pass
